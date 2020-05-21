9c5a7f4272bdd7db79e13153314c0e6c1f5e5d1d
Travis Vesbach <tav1990@hotmail.com>
2020-05-20 22:16:53 -0500

    Add general gameplay functionality, win-lose conditions, timer, and number images

--- minesweeper/MainWindow.xaml.cs
+++ minesweeper/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Threading;
 
 namespace minesweeper
 {
@@ -26,15 +28,20 @@ namespace minesweeper
         //private string img_mine = "image/mine.png";
         //private string img_flag = "image/flag.png";
         //private string img_timer = "image/timer.png";
-        //private string img_smiley_face = "image/smiley_face.png";
-        //private string img_big_smiley_face = "image/big_smiley_face.png";
-        //private string img_frowney_face = "image/frowny_face.png";
+        private string img_smiley_face = "/images/smiley_face.png";
+        private string img_big_smiley_face = "/images/big_smiley_face.png";
+        private string img_frowney_face = "/images/frowny_face.png";
 
-        private string difficultySetting = null;
-        private int timeCount = 0;
-        private int mineCount = 0;
-        private int mapSize = 0;
-        private int totalMines = 0;
+        private string difficultySetting { get; set; }
+        private int timeCount { get; set; }
+        private int mineCount { get; set; }
+        private int mapSize { get; set; }
+        private int totalMines { get; set; }
+        private string status { get; set; }
+        private int revealedCount { get; set; }
+        private int revealedWinCount { get; set; }
+        private DispatcherTimer timer = new DispatcherTimer();
+        private DateTime timerStart { get; set; }
         public ObservableCollection<MineSquare> mineSquares = new ObservableCollection<MineSquare>();
 
 
@@ -46,10 +53,8 @@ namespace minesweeper
             difficultySetting = "beginner";
             mapSize = difficulty[0,0];
             totalMines = difficulty[0,1];
-            mineCount = totalMines;
-            tbMineCount.Text = mineCount.ToString("D3");
-            tbTimeCount.Text = "000";
-            DrawMineField();
+
+            ResetMineField();
 
             DataContext = this;
         }
@@ -59,9 +64,26 @@ namespace minesweeper
             get { return mineSquares;  }
         }
 
+        public void ResetMineField()
+        {
+            UpdateStatus("ready");
+            mineCount = totalMines;
+            revealedCount = 0;
+            revealedWinCount = (mapSize * mapSize) - totalMines;
+            DrawMineField();
+            SetMines();
+            SetAdjacentCount();
+            tbTimeCount.Text = "000";
+            tbMineCount.Text = mineCount.ToString("D3");
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += UpdateTimer;
+
+        }
+
         public void DrawMineField()
         {
-            mineSquares = new ObservableCollection<MineSquare>();
+            mineSquares.Clear();
             foreach (int x in Enumerable.Range(0, mapSize))
             {
                 foreach(int y in Enumerable.Range(0, mapSize))
@@ -74,9 +96,164 @@ namespace minesweeper
                     mineSquares.Add(square);
                 }
             }
-            //CreateGridRowsAndColumns();
         }
 
+        public void SetMines()
+        {
+            List<int[]> positions = new List<int[]>();
+            Random rand = new Random();
+            while (positions.Count() < totalMines)
+            {
+                int x = rand.Next(mapSize);
+                int y = rand.Next(mapSize);
+                int[] position = { x, y };
+                if (!positions.Any(posList => Enumerable.SequenceEqual(position, posList)))
+                {
+                    mineSquares.Where(ms => ms.X == x && ms.Y == y).FirstOrDefault().IsMine = true;
+                    positions.Add(position);
+                }
+
+            }
+        }
+
+        public void SetAdjacentCount()
+        {
+            foreach (MineSquare mineSquare in mineSquares)
+            {
+                int x = mineSquare.X;
+                int y = mineSquare.Y;
+                int adjacentMineCount = 0;
+                for (int x_position = Math.Max(0, x - 1); x_position < Math.Min(x + 2, mapSize); x_position++)
+                {
+                    for (int y_position = Math.Max(0, y - 1); y_position < Math.Min(y + 2, mapSize); y_position++)
+                    {
+                        if (mineSquares.Any(ms => ms.X == x_position && ms.Y == y_position && ms.IsMine))
+                        {
+                            adjacentMineCount++;
+                        }
+                        
+                    }
+                }
+                mineSquare.adjacentCount = adjacentMineCount;
+            }
+        }
+
+        public void ExpandReveal(int x, int y)
+        {
+            for (int x_position = Math.Max(0, x - 1); x_position < Math.Min(x + 2, mapSize); x_position++)
+            {
+                for (int y_position = Math.Max(0, y - 1); y_position < Math.Min(y + 2, mapSize); y_position++)
+                {
+                    MineSquare mineSquare = mineSquares.Where(ms => ms.X == x_position && ms.Y == y_position).FirstOrDefault();
+                    if (mineSquare != null && !mineSquare.IsMine)
+                    {
+                        mineSquare.Reveal();
+                    }
+                }
+            }
+        }
+
+        public void RevealMap()
+        {
+            foreach (MineSquare mineSquare in mineSquares) {
+                if (status == "win")
+                {
+                    mineSquare.Reveal(true);
+                }
+                else
+                {
+                    mineSquare.Reveal();
+                }
+            }
+        }
+
+        public void UpdateStatus(string newStatus)
+        {
+            status = newStatus;
+            if (status == "win")
+            {
+                tbButtonImage.Source = new BitmapImage(
+                                    new Uri(img_big_smiley_face, UriKind.RelativeOrAbsolute));
+            }
+            else if (status == "failed")
+            {
+                tbButtonImage.Source = new BitmapImage(
+                                    new Uri(img_frowney_face, UriKind.RelativeOrAbsolute));
+            }
+            else
+            {
+                tbButtonImage.Source = new BitmapImage(
+                                    new Uri(img_smiley_face, UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        public void UpdateButtonImage()
+        {
+
+            tbButtonImage.Source = new BitmapImage(
+                new Uri("pack://application:,,,/AssemblyName;component/Resources/logo.png"));
+        }
+
+        public void CheckWin(bool revealed = false)
+        {
+            if (status == "ready")
+            {
+                GameStart();
+            }
+            if (status == "playing")
+            {
+                if (revealed)
+                {
+                    revealedCount++;
+                }
+                if (revealedCount == revealedWinCount)
+                {
+                    GameWin();
+                }
+            }
+        }
+
+        public void GameStart()
+        {
+            UpdateStatus("playing");
+            //start timer
+            timer.Start();
+            timerStart = DateTime.Now;
+        }
+
+        public void GameWin()
+        {
+            UpdateStatus("win");
+            timer.Stop();
+            RevealMap();
+            string message = "Congratulations! You beat " + difficultySetting + " in " + (DateTime.Now - timerStart).Seconds.ToString() + " seconds!";
+            MessageBox.Show(message);
+        }
+
+        public void GameOver()
+        {
+            UpdateStatus("failed");
+            timer.Stop();
+            RevealMap();
+        }
+
+        public void UpdateTimer(object sender, EventArgs e)
+        {
+            tbTimeCount.Text = (DateTime.Now - timerStart).Seconds.ToString("D3");
+        }
+
+        public void ClickButton(object sender, EventArgs e)
+        {
+            if (status == "playing")
+            {
+                UpdateStatus("failed");
+                RevealMap();
+            }
+            else if (status == "failed" || status == "win")
+            {
+                ResetMineField();
+            }
+        }
 
     }
 }
--- minesweeper/MineSquare.cs
+++ minesweeper/MineSquare.cs
@@ -20,12 +20,29 @@ namespace minesweeper
         public bool IsMine { get; set; } = false;
         public bool IsFlagged { get; set; } = false;
         public bool IsClickable { get; set; } = true;
+        public int adjacentCount = 0;
         private string img_mine = "images/mine.png";
         private string img_flag = "images/flag.png";
+        private string img_one = "images/1.png";
+        private string img_two = "images/2.png";
+        private string img_three = "images/3.png";
+        private string img_four = "images/4.png";
+        private string img_five = "images/5.png";
+        private string img_six = "images/6.png";
+        private string img_seven = "images/7.png";
+        private string img_eight = "images/8.png";
+        private string img_nine = "images/9.png";
         private MainWindow wnd = (MainWindow)Application.Current.MainWindow;
 
         private ICommand _clickSquare;
         private ICommand _flagSquare;
+
+        public MineSquare()
+        {
+            //NotifyPropertyChanged("Content");
+            //NotifyPropertyChanged("IsClickable");
+            //NotifyPropertyChanged("BackgroundColor");
+        }
 
         public ICommand ClickSquare
         {
@@ -55,18 +72,43 @@ namespace minesweeper
         {
             if (!IsFlagged && !IsRevealed)
             {
+                if (IsMine)
+                {
+                    wnd.GameOver();
+                }
+                else
+                {
+                    Reveal();
+                }
+            }
+        }
+
+        public void Reveal(bool win = false)
+        {
+            if (win && IsMine)
+            {
+                Flag(true);
+            }
+            else if (!IsFlagged && !IsRevealed)
+            {
                 IsClickable = false;
                 IsRevealed = true;
                 BackgroundColor = new SolidColorBrush(Colors.White);
+                SetContent();
+                if (adjacentCount == 0)
+                {
+                    wnd.ExpandReveal(X, Y);
+                }
+                wnd.CheckWin(true);
             }
 
-            this.NotifyPropertyChanged("IsClickable");
-            this.NotifyPropertyChanged("BackgroundColor");
+            NotifyPropertyChanged("IsClickable");
+            NotifyPropertyChanged("BackgroundColor");
         }
 
-        public void Flag()
+        public void Flag(bool win = false)
         {
-            if(IsFlagged)
+            if(IsFlagged && !win)
             {
                 Content = null;
                 IsFlagged = false;
@@ -76,15 +118,60 @@ namespace minesweeper
                 IsFlagged = true;
                 Content = img_flag;
             }
-            this.NotifyPropertyChanged("Content");
+            NotifyPropertyChanged("Content");
+            wnd.CheckWin();
+        }
+
+        public void SetContent()
+        {
+            if (IsMine)
+            {
+                Content = img_mine;
+            }
+            else
+            {
+                switch (adjacentCount)
+                {
+                    case 1:
+                        Content = img_one;
+                        break;
+                    case 2:
+                        Content = img_two;
+                        break;
+                    case 3:
+                        Content = img_three;
+                        break;
+                    case 4:
+                        Content = img_four;
+                        break;
+                    case 5:
+                        Content = img_five;
+                        break;
+                    case 6:
+                        Content = img_six;
+                        break;
+                    case 7:
+                        Content = img_seven;
+                        break;
+                    case 8:
+                        Content = img_eight;
+                        break;
+                    case 9:
+                        Content = img_nine;
+                        break;
+                }
+            }
+            NotifyPropertyChanged("Content");
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void NotifyPropertyChanged(string propName)
         {
-            if (this.PropertyChanged != null)
-                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
         }
 
     }
